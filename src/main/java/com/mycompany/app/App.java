db22e1e4af27a043c7a1c819836d6978c0d597d2
andydevop <56658287+andydevop@users.noreply.github.com>
2019-11-04 17:08:07 -0600

    App by Shreya! Commit#3
    App by Shreya! Commit#3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Genius APP by Anirudh! Version 2.0" );
+        System.out.println( "Genius APP by Shreya! Bery Smart! Version 3.0" );
     }
 }
